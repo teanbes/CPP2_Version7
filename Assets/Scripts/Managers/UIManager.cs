c7003ac75e4041d79846dabd1c757c4ad93b2985
teanbes <teanbes@gmail.com>
2023-05-16 12:37:02 -0400

    Scenes

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[DefaultExecutionOrder(-1)]
+public class UIManager : MonoBehaviour
+{
+    [Header("Button")]
+    public Button playButton;
+    public Button returnToMenuButton;
+    public Button backButton;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
